416765dcb1c3fdfc61409cab26cbf6a195a51d05
Md Golam Jakaria <58362149+MdGolamJakaria@users.noreply.github.com>
2020-03-20 02:44:43 +0600

    unlocked

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAP-i6npaHoH3wVenyZNom9x1h09CibsdM",
+    authDomain: "ema-jhon-simple-4cbc2.firebaseapp.com",
+    databaseURL: "https://ema-jhon-simple-4cbc2.firebaseio.com",
+    projectId: "ema-jhon-simple-4cbc2",
+    storageBucket: "ema-jhon-simple-4cbc2.appspot.com",
+    messagingSenderId: "792313285572",
+    appId: "1:792313285572:web:9df6dfe89abb2edae73291"
+  };
+  export default firebaseConfig;
